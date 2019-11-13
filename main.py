603e1b993a4e95ddf5698de8dddab6214ed2689a
__Gulepin__ <odorofofofo@gmail.com>
2019-11-13 18:37:13 +0300

    Вторая задача

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('1')
+import sys, random
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtGui import QPainter, QColor, QMouseEvent
+from PyQt5.QtCore import Qt
+
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+
+app = QApplication(sys.argv)
+w = MyWidget()
+sys.exit(app.exec_())
