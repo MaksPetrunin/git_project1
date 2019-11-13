b55704b50fd64cfd53cb7537f2e3397f93b8ac76
__Gulepin__ <odorofofofo@gmail.com>
2019-11-13 18:42:45 +0300

    Вторая задача

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys, random
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QPainter, QColor, QMouseEvent
-from PyQt5.QtCore import Qt
 
 
 
@@ -10,6 +9,16 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.show()
+
+    def run(self):
+        a = random.randint(10, 200)
+        self.paint = QPainter()
+        self.paint.begin(self)
+        self.paint.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        self.paint.drawEllipse(400, 300, a, a)
+        self.paint.end()
 
 
 app = QApplication(sys.argv)
